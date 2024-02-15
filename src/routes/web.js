d5d6fb98800a369d86d4b3e5063048b3bf3f733f
Nguyen Dat Trong <trongnd106@gmail.com>
2024-02-15 21:28:17 +0700

    link fe

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,7 @@
 import express from "express"
-import * as homeController 
-    from "../controller/homeController.js"
+import * as homeController from "../controller/homeController.js"
+import * as apiController from "../controller/apiController.js"
+
 const router = express.Router()
 
 /**
@@ -15,6 +16,8 @@ const initWebRoute = (app) => {
     router.post('/delete-user/:id', homeController.handleDeleteUser)
     router.get('/update-user/:id', homeController.getUpdateUserPage)
     router.post('/user/update-user', homeController.handleUpdateUser)
+
+    router.get('/api/test-api', apiController.testApi)
     
     return app.use("/", router)
 }
--- src/server.js
+++ src/server.js
@@ -9,6 +9,28 @@ import connection from "./config/connectDB.js"
 
 const app = express()
 
+const PORT = process.env.PORT || 8080
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // config view engine
 configViewEngine(app)
 
@@ -21,8 +43,6 @@ connection()
 
 // init web routes
 initWebRoute(app)
-
-const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log(">>> JWT Backend is running on the port = ", PORT)
